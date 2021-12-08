df09207e31275725c9dbdeb80a16954f04726347
sulfuroussquares <bbetancova@gmail.com>
2021-12-08 07:23:04 -0800

    display basic information in results area

--- browser-extension/tabs.js
+++ browser-extension/tabs.js
@@ -8,6 +8,21 @@ function pasteQuery() {
 
 document.addEventListener('DOMContentLoaded', pasteQuery);
 
+// this function accepts an {object} and returns the contents displayed to the page
+function displayResults(jsonData) {
+  // results is an array of n items
+  let results = jsonData['results'];
+
+  results.forEach((element) => {
+    let item = JSON.stringify(element);
+    // you can now access element.body, element.lecture, element.timestamp, element.week
+    document.getElementById('results-area').innerText = element.body;
+
+    // create a new div area for each element
+    // insert into that div, elements for each body/lecture/timestamp/week
+  });
+}
+
 console.log('made it to tabs');
 // test hitting our local server
 fetch('http://localhost:5000/test')
@@ -16,6 +31,13 @@ fetch('http://localhost:5000/test')
   })
   .then(function (jsonResponse) {
     console.log(jsonResponse);
+    // document.getElementById('results-area').innerText = JSON.stringify(
+    //   jsonResponse['results'][0]
+    // );
+
+    // document.getElementById('results-area').innerText =
+    //   JSON.stringify(jsonResponse);
+    displayResults(jsonResponse);
   })
   .catch(function (error) {
     console.log('Error: ' + error);
